830b5ffcbdc1d7a085ea84b0f3a270e79c09a421
borisCai <cdc2011@126.com>
2018-08-26 18:26:00 +0800

    Update qxc2.js

--- qxc2.js
+++ qxc2.js
@@ -3,7 +3,7 @@ var date="";
 if(name=='aqaqa'){
  date="2118-01-03";
 }else if (name=='ca2371a2-d83f-4dc1-8b0d-fdf995158539'){
- date="2018-08-11";
+ date="2018-08-01";
 } else if (name=='802c6c8e-153f-45cb-8710-2a6f02557230'){
  date="2018-10-01";
 } ;
