6b5941454aa1385ce1fb01b1925d311966675238
borisCai <cdc2011@126.com>
2018-08-27 16:51:23 +0800

    Update qxc2.js

--- qxc2.js
+++ qxc2.js
@@ -1,9 +1,12 @@
-function redate(name){
+function redate(name,hyname){
 var date="2001-01-01";
-if(name=='d040b253-263c-40a6-9a7e-d41dad42ce9b'){
+if(name=='d040b253-263c-40a6-9a7e-d41dad42ce9b' ){ 
  date="2118-01-03";
+ }
 }else if (name=='1036015f-f697-4b83-bf99-11ef8a253793'){
+ if(hyname=='chy99'){
  date="2018-10-01";
+ }
 } else if (name=='ca2371a2-d83f-4dc1-8b0d-fdf995158539'){
  date="2018-11-01";
 } ;
